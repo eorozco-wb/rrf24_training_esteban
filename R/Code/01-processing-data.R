e9a42231482b54e7551e8684708ccbbb10e802a3
eorozco-wb <eorozco@worldbank.org>
2024-10-01 12:17:04 -0400

    Adding processing lab exercise
    -Encoding variables
    -Cleaning and tidying data

--- R/Code/01-processing-data.R
+++ R/Code/01-processing-data.R
@@ -2,45 +2,62 @@
 # 01. Data processing
 
 ### Libraries
-# library(haven)
-# library(dplyr)
-# library(tidyr)
-# library(stringr)
-# library(labelled)
+
+install.packages("haven")
+install.packages("dplyr")
+install.packages("tidyr")
+install.packages("stringr")
+install.packages("labelled")
+
+library(haven)
+library(dplyr)
+library(tidyr)
+library(stringr)
+library(labelled)
 
 ### Loading data ----
 
 # Load the dataset
-data_path <- "ADD-YOUR-PATH"
+data_path <- "/Users/estebanorozco/Documents/GitHub/rrf24_training_esteban/R/DataWork/Data"
 data      <- read_dta(file.path(data_path, "Raw/TZA_CCT_baseline.dta"))
+
+#We checked the data for duplicates and found two in hhid, so we removed them.
+
 
 ### Remove duplicates based on hhid
 data_dedup <- data %>%
-    ......
+    distinct(hhid, .keep_all=TRUE)
 
 ### Household (HH) level data ----
 
 #### Tidying data for HH level
 data_tidy_hh <- data_dedup %>%
-    ......
+    select(1:15,36:53)
 
 ### Data cleaning for Household-member (HH-member) level
 data_clean_hh <- data_tidy_hh %>%
     # Convert submissionday to date
-    mutate(...... = as.Date(......, format = "%Y-%m-%d %H:%M:%S")) %>%
+    mutate(submissiondate= as.Date(submissionday, format = "%Y-%m-%d %H:%M:%S")) %>%
     # Convert duration to numeric (if it is not already)
-    mutate(......) %>%
+    mutate(duration=as.numeric(duration)) %>%
     # Convert ar_farm_unit to factor (categorical data)
-    mutate(......) %>%
+    mutate(ar_farm_unit=as.factor(ar_farm_unit)) %>%
+    mutate(ar_farm_unit=na_if(ar_farm_unit,"")) %>% 
+    #Clean the crop other variable (capitalize first letetr)
+    mutate(crop_ther = str_to_title(crop_other)) %>% 
     # Replace values in the crop variable based on crop_other using regex for new crops
     mutate(crop = case_when(
-        ......
+        str_detect(crop_other, "Coconut") ~ 40,
+        str_detect(crop_other, "Sesame") ~ 41,
+        TRUE ~ crop
     )) %>%
     # Recode negative numeric values (-88) as missing (NA)
-    mutate(across(......)) %>%
+    mutate(across(where(is.numeric),~replace(., . ==-88, NA))) %>%
     # Add variable labels
     set_variable_labels(
-        ......
+     duration="Duration of the interview (minutes)",
+     submissiondate= "Submission Date",
+     
     )
 
 # Save the household data
@@ -50,18 +67,19 @@ write_dta(data_clean_hh, file.path(data_path, "Intermediate/TZA_CCT_HH.dta"))
 
 #### Tidying data for HH-member level
 data_tidy_mem <- data_dedup %>%
-    select(......,
-           starts_with(......)) %>%
+    select(vid:enid, starts_with("gender"),starts_with("age"),starts_with("read"),
+           starts_with("clinic_visit"), starts_with("sick"), starts_with("days_sick"),
+           starts_with("treat_fin")) %>%
     pivot_longer(cols = -c(vid, hhid, enid),  # Keep IDs static
-                 names_to = ......,
+                 names_to = c(".value","member"),
                  names_pattern = "(.*)_(\\d+)")  # Capture the variable and the suffix
 
 ### Data cleaning for HH-member level
 data_clean_mem <- data_tidy_mem %>%
     # Drop rows where gender is missing (NA)
-    ...... %>%
+    filter(!is.na(gender)) %>%
     # Variable labels
-    ......
+    set_variable_labels()
 
 # Save the tidy household-member data
 write_dta(data_clean_mem, file.path(data_path, "Intermediate/TZA_CCT_HH_mem.dta"))
@@ -73,9 +91,9 @@ secondary_data <- read.csv(file.path(data_path, "Raw/TZA_amenity.csv"))
 
 # Tidying data
 secondary_data <- secondary_data %>%
-    pivot_wider(names_from = ......,
-                values_from = ......,
-                names_prefix = ......)
+    pivot_wider(names_from = amenity,
+                values_from = n,
+                names_prefix = "")
 
 # Save the final tidy secondary data
 write_dta(secondary_data, file.path(data_path, "Intermediate/TZA_amenity_tidy.dta"))
